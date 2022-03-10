4e8093ac99177352054f3cbcfc14d8b7885a4e9e
rdewsnap <54950510+rdewsnap@users.noreply.github.com>
2022-03-09 21:57:46 -0500

    Push!

--- CDR/ml.py
+++ CDR/ml.py
@@ -8,8 +8,9 @@ from sklearn.metrics import classification_report, confusion_matrix
 import pickle
 import serial
 import os
+from statistics import mode
 
-training_path=r"C:\Users\Ryan\Documents\GitHub\SDP\CDR\CDR_official_test\FULL_DATA.csv"
+training_path=r"C:\Users\Ryan\Documents\GitHub\SDP\CDR\CDR_official_test\rms_3_gestures.CSV"
 
 names= ['input1', 'input2', 'input3', 'class']
 
@@ -25,7 +26,7 @@ scaler.fit(X_train)
 
 X_train = scaler.transform(X_train)
 
-classifier = KNeighborsClassifier(n_neighbors=5)
+classifier = KNeighborsClassifier(n_neighbors=20)
 classifier.fit(X_train, y_train)
 
 port = 'COM7'
@@ -34,20 +35,29 @@ ser = serial.Serial(port, 9600, timeout=10)  ##(port, 115200, timeout=10)
 
 os.system('cls')
 
-prev =['none']
+'''prev =['none']
 while True:
-    dat = ser.readline().decode('utf-8', 'ignore').strip('\r\n').split(", ")
-    temp = [int(num) for num in dat]
+    dat = ser.readline().decode('utf-8', 'ignore').strip('\r\n').split(",")
+    temp = [int(num.lstrip()) for num in dat]
     vals = np.asarray(temp).reshape(1,-1)
-'''x = [None] * 20
+    if vals.any() > 0:
+        vals = scaler.transform(vals)
+        y_pred = classifier.predict(vals)
+        if y_pred != prev:
+            print(y_pred)
+        prev = y_pred'''
+x = [None] * 20
 y = [None] * 20
 z = [None] * 20
+
+prediction = [None] * 5
 counter = 0
+counter_pred = 0
 
 prev = ['none']
 while True:
     dat = ser.readline().decode('utf-8', 'ignore').strip('\r\n').split(", ")
-    temp = [int(num) for num in dat]
+    temp = [int(num.lstrip()) for num in dat]
     x[counter] = temp[0]
     y[counter] = temp[1]
     z[counter] = temp[2]
@@ -57,14 +67,17 @@ while True:
         rms_input2 = np.sqrt(sum(map(lambda i: i * i, y)) / 20)
         rms_input3 = np.sqrt(sum(map(lambda i: i * i, z)) / 20)
         rms_final = np.array([rms_input1, rms_input2, rms_input3]).reshape(1, -1)
-        print(rms_final)
+        rms_final = scaler.transform(rms_final)
         y_pred = classifier.predict(rms_final)
-        #print(y_pred)
+        if y_pred != prev:
+            print(y_pred)
+        prev = y_pred
+
         counter = 0
         x = [None] * 20
         y = [None] * 20
         z = [None] * 20
-'''
+
 
 
 
